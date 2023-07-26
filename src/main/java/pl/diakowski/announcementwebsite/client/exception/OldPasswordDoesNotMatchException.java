d2e278be5fe75c66cda013ef44f94c3350673f56
Dekrate <mikdiakowski@interia.pl>
2023-07-26 10:31:00 +0200

    fix: Fixed bugged changing password method in ClientService v2

--- src/main/java/pl/diakowski/announcementwebsite/client/exception/OldPasswordDoesNotMatchException.java
+++ src/main/java/pl/diakowski/announcementwebsite/client/exception/OldPasswordDoesNotMatchException.java
@@ -1,16 +1,4 @@
 package pl.diakowski.announcementwebsite.client.exception;
 
-import pl.diakowski.announcementwebsite.client.ClientService;
-
-/**
- * Exception thrown when old password does not match the one in a database
- * @see ClientService#changePassword
- */
-public class OldPasswordDoesNotMatchException extends SecurityException {
-	/**
-	 * @param message message to be displayed
-	 */
-	public OldPasswordDoesNotMatchException(String message) {
-		super(message);
-	}
+public class OldPasswordDoesNotMatchException extends Exception {
 }
