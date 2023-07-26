3d04b9e8b7159385b772892ea77e4fce0f5669e5
Dekrate <mikdiakowski@interia.pl>
2023-07-26 10:45:18 +0200

    fix: Added client panel. It includes showing all announcements

--- src/main/java/pl/diakowski/announcementwebsite/client/exception/OldPasswordDoesNotMatchException.java
+++ src/main/java/pl/diakowski/announcementwebsite/client/exception/OldPasswordDoesNotMatchException.java
@@ -1,4 +1,16 @@
 package pl.diakowski.announcementwebsite.client.exception;
 
-public class OldPasswordDoesNotMatchException extends Exception {
+import pl.diakowski.announcementwebsite.client.ClientService;
+
+/**
+ * Exception thrown when old password does not match the one in a database
+ * @see ClientService#changePassword
+ */
+public class OldPasswordDoesNotMatchException extends SecurityException {
+	/**
+	 * @param message message to be displayed
+	 */
+	public OldPasswordDoesNotMatchException(String message) {
+		super(message);
+	}
 }
--- src/main/java/pl/diakowski/announcementwebsite/web/ClientController.java
+++ src/main/java/pl/diakowski/announcementwebsite/web/ClientController.java
@@ -16,6 +16,10 @@ import pl.diakowski.announcementwebsite.client.exception.PasswordsDoNotMatchExce
 import pl.diakowski.announcementwebsite.contactmethod.ContactMethodService;
 import pl.diakowski.announcementwebsite.contactmethod.dto.ContactMethodDto;
 
+/**
+ * Controller for client pages
+ * @since 1.0
+ */
 @Controller
 public class ClientController {
 	private final ClientService clientService;
@@ -33,14 +37,23 @@ public class ClientController {
 		this.contactMethodService = contactMethodService;
 	}
 
+	/**
+	 * Method for redirecting to /client
+	 * @param model Model for redirecting
+	 * @return RedirectView to /client
+	 */
 	@GetMapping("/client")
 	public String client(Model model) {
 		ClientDto client = getClientDto();
 		model.addAttribute("client", client);
-		model.addAttribute("announcements", announcementService.findAllByClient(client, 0)); // page 0 implies the first page
 		return "client";
 	}
 
+	/**
+	 * Method for redirecting to /client/announcements
+	 * @param model Model for redirecting
+	 * @return RedirectView to /client/announcements
+	 */
 	@GetMapping("/client/announcements")
 	public String announcements(Model model) {
 		ClientDto client = getClientDto();
@@ -49,6 +62,11 @@ public class ClientController {
 		return "user-announcements";
 	}
 
+	/**
+	 * Method for redirecting to /client/announcements
+	 * @param model Model for redirecting
+	 * @return RedirectView to /client/contact-method
+	 */
 	@GetMapping("/client/contact-method")
 	public String contactMethod(Model model) {
 		ClientDto client = getClientDto();
@@ -58,12 +76,19 @@ public class ClientController {
 
 	/**
 	 * @return ClientDto of currently logged-in user
+	 * @see ClientDto
 	 */
 	private ClientDto getClientDto() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		return clientService.findByUsername(authentication.getName());
 	}
 
+	/**
+	 * @param contactMethodDto Dto with new contact method
+	 * @param model Model for redirecting
+	 * @return RedirectView to /client/contact-method
+	 * @see ContactMethodDto
+	 */
 	@PostMapping("/client/contact-method")
 	public RedirectView changeContactMethod(ContactMethodDto contactMethodDto, Model model) {
 		RedirectView redirectView = new RedirectView();
@@ -75,25 +100,42 @@ public class ClientController {
 		return redirectView;
 	}
 
+	/**
+	 * Method for redirecting to /client/contact-method
+	 * @param model Model for redirecting
+	 * @return RedirectView to /client/contact-method
+	 */
 	@GetMapping("/client/change-password")
 	public String changePasswordWebsite(Model model) {
 		ClientDto client = getClientDto();
+		model.addAttribute("error", model.getAttribute("error"));
 		model.addAttribute("client", client);
 		return "change-password";
 	}
 
-	// TODO post mappings
+	/**
+	 * Method created to allow a user to change his password.<br />
+	 * It checks if the old password matches the one in the database.<br />
+	 * It also checks if the new password matches the repeated new password.<br />
+	 * If any of the checks fail, it redirects to /client/change-password with an error message.<br />
+	 * If all checks pass, it redirects to /client/change-password with a success message.<br />
+	 * @param model Model for redirecting
+	 * @param oldPassword Old password
+	 * @param newPassword New password
+	 * @param repeatedNewPassword Repeated new password
+	 * @return RedirectView to /client/change-password
+	 */
 	@PostMapping("/client/change-password")
-	public RedirectView changePassword(Model model, String oldPassword, String newPassword, String repeatedNewPassword) {
+	public String changePassword(Model model, String oldPassword, String newPassword, String repeatedNewPassword) {
 		ClientDto client = getClientDto();
-		RedirectView redirectView = new RedirectView();
 		try {
 			clientService.changePassword(client, oldPassword, newPassword, repeatedNewPassword);
+			model.addAttribute("success", true);
 		} catch (OldPasswordDoesNotMatchException e) {
 			model.addAttribute("error", "Old password does not match");
 		} catch (PasswordsDoNotMatchException e) {
 			model.addAttribute("error", "Passwords do not match");
 		}
-		return null; // TODO return redirectView
+		return "change-password";
 	}
 }
